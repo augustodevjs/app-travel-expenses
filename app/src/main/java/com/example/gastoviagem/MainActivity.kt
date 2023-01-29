4bb358442041f84095a7085907c304be7d4fb580
augusto <augustosaaude@gmail.com>
2023-01-29 16:58:20 -0300

    feat: adicionei o evento de click na MainActivity.kt

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -2,13 +2,27 @@ package com.example.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        Toast.makeText(this, "Fui clicado", Toast.LENGTH_LONG).show()
     }
 }
